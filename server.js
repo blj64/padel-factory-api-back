20ee0623a5d47ef624b4d19760e70d70621e9b1b
blj64 <jliogerbun@gmail.com>
2024-02-13 21:29:42 +0100

    suppression note

--- server.js
+++ server.js
@@ -30,7 +30,6 @@ app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/', require('./routes/root'))
 app.use('/auth', require('./routes/authRoutes'))
 app.use('/users', require('./routes/userRoutes'))
-app.use('/notes', require('./routes/noteRoutes'))
 app.use('/tournaments', require('./routes/tournamentRoutes'))
 
 
